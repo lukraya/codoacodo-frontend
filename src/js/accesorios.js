9ce53936f572f18762ce337921cab9ef54a3891e
Lucía Krayacich <luciakrayacich@gmail.com>
2022-10-13 23:25:21 -0300

    Correccion: faltaba usar js para dibujar el contenido del aside. TambiÃen me deshice de div extra.

--- src/js/accesorios.js
+++ src/js/accesorios.js
@@ -1,6 +1,7 @@
 //https://fakestoreapi.com/
 let cardsContainer = document.getElementById('contenedor-cards')
-console.log(cardsContainer)
+let listWrapper = document.getElementById('list-wrapper')
+//console.log(cardsContainer)
 
 async function getProducts () {
     try {
@@ -25,9 +26,9 @@ async function renderProducts () {
         products[2].name = 'Anillo de Oro Blanco'
         products[3].name = 'Aros de Oro Rosa'
 
-        /* Preparo un string que contiene la etiqueta inicial del elemento que voy a agregar después al contenedor de
-        cards */
-        let div = '<div class="wrapper-cards">'
+        //CARDSCONTAINER
+        /* Preparo un string para agrupar los cards y agregar al contenedor */
+        let div = ''
         //Itero el array de productos para crear un div con clase card y concatenarlo a mi string inicial por cada uno
         products.forEach(prod => {
             div += `<div class="card">
@@ -35,23 +36,24 @@ async function renderProducts () {
                         <p>`+prod.name+`</p>
                     </div>`
         });
-        //Finalmente sumo al string la etiqueta que cierra
-        div+='</div>'
+        //Finalmente sumo otro string vacío para cerrar el agrupamiento
+        div+=''
         //E inserto el string como innerhtml de mi contenedor
         cardsContainer.innerHTML = div
-        /*     `<div class="card">
-                <img src="`+products[0].image+`" alt="`+products[0].title+`">
-                <p>`+products[0].name+`</p>
-            </div>` */
+
+
+        //LISTWRAPPER
+        let str = ''
+        products.forEach(prod=>{
+            str += '<li><a href="#">'+prod.name+'</a></li>'
+        })
+        str+=''
+        listWrapper.innerHTML = str
+
     } catch (error) {
         console.log(error)
     }
 }
 
-{/* `<p>{products[0].title}</p>`
-<div class="card">
-                <img src="" alt="imagen de item1">
-                <p>Item 1</p>
-            </div> */}
 
 renderProducts()
